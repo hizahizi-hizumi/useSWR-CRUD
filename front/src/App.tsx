4280057479ade578456313a3b3ecb5f2ec44d899
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-09-13 21:17:49 +0900

    dev: 動作確認

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useSWR from "swr"
+
+async function fetcher(url: string) {
+  const response = await fetch(url)
+  const result = await response.json()
+
+  return result
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { data } = useSWR("https://jsonplaceholder.typicode.com/todos/1", fetcher)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.title}
     </>
   )
 }
