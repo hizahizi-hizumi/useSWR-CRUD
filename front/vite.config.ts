50174255effedbaa858ddbd8ae8fd91364d85507
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-09-14 17:06:01 +0900

    chore: set up generouted

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,11 @@
+import generouted from "@generouted/react-router/plugin"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), generouted()],
   resolve: {
-    alias: [
-      { find: '@', replacement: `${__dirname}/src` },
-    ]
-  }
+    alias: [{ find: "@", replacement: `${__dirname}/src` }],
+  },
 })
