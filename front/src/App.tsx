c436f9464fe451d80f0ddc0779021ef93bfa6440
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-09-14 12:11:22 +0900

    dev: 動作確認

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,8 +12,24 @@ async function fetcher(url: string) {
   return result
 }
 
+async function post() {
+  const response = await fetch("/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: "Task 3" }),
+  })
+  const result = await response.json()
+
+  return result
+}
+
 function App() {
   const { data } = useSWR("/tasks", fetcher)
+
+  post().then((result) => console.log(result))
+
   return (
     <>
       {data && data.map((task: Task) => { return <div key={task.id}>{task.title}</div> })}
