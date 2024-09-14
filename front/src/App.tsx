adf77aa9fabb2f47854cb09dc22ccbe9ac053e5e
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-09-14 15:09:20 +0900

    remove: 動作確認用の記述を削除

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,62 +1,11 @@
-import useSWR from "swr"
-
 import { setupMsw } from "./mocks/setupMsw"
-import type { Task } from "./types/task"
 
 setupMsw()
 
-async function fetcher(url: string) {
-  const response = await fetch(url)
-  const result = await response.json()
-
-  return result
-}
-
-async function post() {
-  const response = await fetch("/tasks", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title: "Task 3" }),
-  })
-  const result = await response.json()
-
-  return result
-}
-
-async function put() {
-  const response = await fetch("/tasks/1", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title: "Task 1 updated" }),
-  })
-  const result = await response.json()
-
-  return result
-}
-
-async function delete_() {
-  const response = await fetch("/tasks/2", {
-    method: "DELETE",
-  })
-  const result = await response.json()
-
-  return result
-}
-
 function App() {
-  const { data } = useSWR("/tasks", fetcher)
-
-  post().then((result) => console.log(result))
-  put().then((result) => console.log(result))
-  delete_().then((result) => console.log(result))
-
   return (
     <>
-      {data && data.map((task: Task) => { return <div key={task.id}>{task.title}</div> })}
+      hoge
     </>
   )
 }
